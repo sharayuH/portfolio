331d55357eead07a48d4380c8f70167fd9d947bc
Sharayu Hiwarkhedkar <hiwarkhedkarsharayu@gmail.com>
2023-03-24 15:17:55 +0530

    update: twitter username

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'Sharayu Hiwarkhedkar is a computer science enthusiast.',
     siteUrl: 'https://sharayu-h.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@SharayuHiwarkh1',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
